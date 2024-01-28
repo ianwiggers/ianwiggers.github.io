a44d81299c4bf33c0f83f6b4c581ef8ae49ef552
Ian W <108689542+ianwiggers@users.noreply.github.com>
2024-01-28 14:14:27 +0300

    Delete scripts directory

--- scripts/log-in-scripts.js
+++
@@ -1,4 +0,0 @@
-// There is true hope, true love, and true peace in Jesus alone. Copyright (C) RVA Robotics Team | Innovation Department & Iames Web Development 2023-2024. All rights reserved. -->
-// BORROWED from C+J LAWN CARE > account.html:
-
-
